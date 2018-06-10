a21e7d21f062c4b0029c26f635a068560e498736
soumyadsanyal <soumya@soumyadsanyal.com>
2018-06-10 02:18:26 -0400

    testclass name

--- 771/src/test/scala/SolutionTest.scala
+++ 771/src/test/scala/SolutionTest.scala
@@ -1,4 +1,4 @@
-class CubeCalculatorTest extends org.scalatest.FunSuite {
+class 711Test extends org.scalatest.FunSuite {
   test("Solution.numJewelsInStones") {
     assert(Solution.numJewelsInStones("soumya", "sanyal") === 4);
     assert(Solution.numJewelsInStones("sanyal", "soumya") === 3);
