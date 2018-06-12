46c6eb166f2922aaef3d855eec1a4a494b43d352
soumyadsanyal <soumya@soumyadsanyal.com>
2018-06-12 19:00:40 -0400

    some FunSuite

--- 771/src/test/scala/SolutionTest.scala
+++ 771/src/test/scala/SolutionTest.scala
@@ -3,4 +3,13 @@ class SolutionTest extends org.scalatest.FunSuite {
     assert(Solution.numJewelsInStones("soumya", "sanyal") === 4);
     assert(Solution.numJewelsInStones("sanyal", "soumya") === 3);
   }
+  test("Solution.trivial") {
+    assert(Solution.numJewelsInStones("", "sanyal") === 0);
+    assert(Solution.numJewelsInStones("sanyal", "") === 0);
+    assert(Solution.numJewelsInStones("", "") === 0);
+  }
+  test("Solution.repeats") {
+    val repeats = 10
+    assert(Solution.numJewelsInStones("soumya", "sanyal"*repeats) === repeats*4);
+  }
 }
